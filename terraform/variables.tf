a151bc3f12a400ea7fdab2cfaae464e6c1783e0e
Haris367 <kharis963@gmail.com>
2025-09-13 23:21:56 +0500

    removed terraform folder from repo

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
-variable "lambda_function_name" {
-  description = "Lambda function name"
-  default     = "SentimentAnalysisLambda"
-}
